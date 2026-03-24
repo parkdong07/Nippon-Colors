da5f93628c12be28778e8417fa543b4eaad50afc
parkdong07 <36270702+parkdong07@users.noreply.github.com>
2026-03-24 14:56:13 +0700

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import {defineConfig, loadEnv} from 'vite';
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
+    base: './',
     plugins: [react(), tailwindcss()],
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
@@ -19,14 +20,6 @@ export default defineConfig(({mode}) => {
       // HMR is disabled in AI Studio via DISABLE_HMR env var.
       // Do not modifyâfile watching is disabled to prevent flickering during agent edits.
       hmr: process.env.DISABLE_HMR !== 'true',
-
-      // ในไฟล์ vite.config.ts
-  export default defineConfig(({mode}) => {
-  // ... โค้ดเดิม ...
-  return {
-    base: 'https://parkdong07.github.io/Nippon-Colors/', // <--- เพิ่มบรรทัดนี้ (เช่น '/nippon-colors/')
-    plugins: [react(), tailwindcss()],
-    // ... โค้ดเดิม ...
     },
   };
 });
